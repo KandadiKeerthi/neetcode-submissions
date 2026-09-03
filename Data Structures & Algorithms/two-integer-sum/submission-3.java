2ce798be1e1bd901e2a3e01160c2be52cf6e83cf
Keerthi Kandadi <64891151+KandadiKeerthi@users.noreply.github.com>
2026-09-02 23:03:35 -0500

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // for (int i = 0; i <nums.length; i++) {
+        //     for (int j = i+1; j<nums.length;j++) {
+        //         if (nums[i] + nums[j] == target) {
+        //             return new int[]{i,j};
+        //         }
+        //     }
+        // }
+        // return new int[]{};
+
+        
+        // int n = nums.length;
+        // int[][] pairs = new int[n][2];
+        // for (int i = 0; i<n;i++) {
+        //     pairs[i][0] = nums[i];
+        //     pairs[i][1] = i;
+        // }
+        // Arrays.sort(pairs, (a,b)->Integer.compare(a[0],b[0]));
+        // int l=0, r=n-1;
+        // while (l<r) {
+        //     int sum = pairs[l][0] + pairs[r][0];
+        //     if (sum == target) {
+        //         int i = pairs[l][1], j = pairs[r][1];
+        //         return new int[]{Math.min(i,j), Math.max(i,j)}; 
+        //         } else if (sum < target) {
+        //             l++;
+        //         } else {
+        //             r--;
+        //         }
+        // } 
+        // return new int[]{};
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i<nums.length;i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+}
